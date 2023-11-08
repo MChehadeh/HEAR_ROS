90c47fcb62fdd9a6833896508be94221ed453a3c
mchehadeh <mchehadeh@ku.ac.ae>
2023-11-08 15:24:46 +0400

    modified:   src/Interface_motive_natnet/NatNetMotiveController.cpp

--- src/Interface_motive_natnet/NatNetMotiveController.cpp
+++ src/Interface_motive_natnet/NatNetMotiveController.cpp
@@ -18,11 +18,11 @@ void NatNetMotiveController::callbackPerform(std::tuple<size_t, char *> data){
     std::vector<OptiTrackRigidBodyData> rigid_bodies;
     Unpack((char*)std::get<1>(data),rigid_bodies);
     rigid_bodies_shmem.writeSharedData(rigid_bodies);
-    for (auto rgbdy : rigid_bodies){
-        std::cout << "Rigid Body received, id:" << rgbdy.id << ", pos: " <<rgbdy.pos.x << ","<<
-    rgbdy.pos.y << ","<< rgbdy.pos.z << ", quat: " << rgbdy.quat.getW() << ","<< 
-    rgbdy.quat.getX() << ","<< rgbdy.quat.getY() << ","<< rgbdy.quat.getZ() <<std::endl;
-    }
+    // for (auto rgbdy : rigid_bodies){
+    //     std::cout << "Rigid Body received, id:" << rgbdy.id << ", pos: " <<rgbdy.pos.x << ","<<
+    // rgbdy.pos.y << ","<< rgbdy.pos.z << ", quat: " << rgbdy.quat.getW() << ","<< 
+    // rgbdy.quat.getX() << ","<< rgbdy.quat.getY() << ","<< rgbdy.quat.getZ() <<std::endl;
+    // }
     
 }
 
